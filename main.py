2b3d0d6fdf19f91a2330dbb759cd5e428f0166c0
Cloud <smirnovceo@yandex.ru>
2021-10-15 18:32:05 +0500

    Refactoring naming, typehints, lesscode

--- main.py
+++ main.py
@@ -5,34 +5,30 @@ from pandas import read_excel
 
 
 @lru_cache
-def get_resumes():
+def parse_table(table_path: str) -> list:
     columns = [
         'user_id',
         'doc_id',
     ]
-    resumes = read_excel('test_frame.xlsx', usecols=columns,
-                         keep_default_na=False).to_dict(orient='records')
-    return resumes
+    users_and_docs = read_excel(table_path, usecols=columns,
+                                keep_default_na=False).to_dict(orient='records')
+    return users_and_docs
 
 
-def get_users_with_resume(total_users):
-    users_with_resume = []
-    for resume in total_users:
-        if resume['doc_id'] != 'NULL':
-            users_with_resume.append(resume['user_id'])
-    return users_with_resume
+def get_users_with_docs(all_users: list) -> list:
+    users_with_docs = []
+    for doc in all_users:
+        if doc['doc_id'] != 'NULL':
+            users_with_docs.append(doc['user_id'])
+    return users_with_docs
 
 
-def count_repeat(users, unic_users):
-    return users - unic_users
-
-
-def main():
-    all_users = get_resumes()
+def main() -> None:
+    all_users = parse_table('test.xlsx')
     all_users_count = len(all_users)
     print(f'{all_users_count} - USER_ID all')
 
-    users_with_docs = get_users_with_resume(all_users)
+    users_with_docs = get_users_with_docs(all_users)
     users_with_docs_count = len(users_with_docs)
     print(f'{users_with_docs_count} - USER_ID with DOC_ID not NULL')
 
@@ -40,7 +36,7 @@ def main():
     unic_users_count = len(unic_users)
     print(f'{unic_users_count} - USER_ID unic')
 
-    repeated_users = count_repeat(users_with_docs_count, unic_users_count)
+    repeated_users = users_with_docs_count - unic_users_count
     print(f'{repeated_users} - USER_ID with more that 1 document')
 
     proportion = repeated_users / all_users_count * 100
